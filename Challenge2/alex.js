dff4cad8e72da45570ef671b2fae9af65f777ca6
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-07 10:55:51 +0200

    fixed the alex file

--- Challenge2/alex.js
+++ Challenge2/alex.js
@@ -1,8 +1,10 @@
 // alex.js
+export const privateFirstName = "Alex";
+export const privateSurname = "Naidoo";
+export const publicRole = "Head of Marketing";
+// private firstname = "Alex"
+// private surname = "Naidoo"
+// public role = "Head of Marketing"
 
-private firstname = "Alex"
-private surname = "Naidoo"
-public role = "Head of Marketing"
-
-private display= firstname + " " + surname + " (" + role + ")"
+const privateDisplay = privateFirstName + " " + privateSurname + " " + publicRole;
 document.querySelector('#alex').innerText = display
