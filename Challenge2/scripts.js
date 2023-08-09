3fd7774d3624e490324ee8607f2f87c71ef3ad59
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-09 13:09:15 +0200

    Changed the imports

--- Challenge2/scripts.js
+++ Challenge2/scripts.js
@@ -1,5 +1,5 @@
 // scripts.js
-import { role as alexRole } from "./alex.js";
-import { role as johannesRole } from "./johannes.js";
-import { role as nwabisaRole } from "./nwabisa.js";
+import { alexRole } from "./alex.js";
+import { johannesRole } from "./johannes.js";
+import { nwabisaRole } from "./nwabisa.js";
 console.log("Roles:", nwabisaRole, johannesRole, alexRole);
