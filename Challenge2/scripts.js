8946187a29d1982edd1c304dc09a9a3d0f0dfca8
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-10 12:57:00 +0200

    changed the exports

--- Challenge2/scripts.js
+++ Challenge2/scripts.js
@@ -1,5 +1,5 @@
 // scripts.js
-import { alexRole } from "./alex.js";
-import { johannesRole } from "./johannes.js";
-import { nwabisaRole } from "./nwabisa.js";
-console.log("Roles:", nwabisaRole, johannesRole, alexRole);
+import { role as alex } from "./alex.js";
+import { role as johannes } from "./johannes.js";
+import { role as nwabisa } from "./nwabisa.js";
+console.log("Roles:", nwabisa, johannes, alex);
